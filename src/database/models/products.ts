03daa018e8141f62d5dc9e1e63b6a9378e508dd3
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-17 02:15:19 -0300

    feat: Products model

---
+++ src/database/models/products.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Products extends Model {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+  public Price!: string;
+}
+
+Products.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize: db,
+    modelName: 'Products',
+    tableName: 'productss',
+  }
+);
+
+export default Products;
