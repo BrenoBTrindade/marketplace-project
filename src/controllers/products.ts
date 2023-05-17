18403a7c92cec0a8742f0d8f616d0cda83386203
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-17 16:42:05 -0300

    feat: deletebyId in ProductController

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -8,9 +8,11 @@ const productService = new ProductService()
 export default class ProductController {
     public async create (req: Request, res: Response) {
         try {
+
             const product = await productService.create(req.body)
             res.status(201).json(product)
         } catch (error: Error | any | unknown) {
+
             console.log('Message error: ', error.message)
             res.status(400).send({message: error.message})
         }
@@ -24,12 +26,27 @@ export default class ProductController {
 
     public async getById (req: Request, res: Response) {
         try {
+
             const {id} = req.params
             const product = await productService.getById(Number(id))
             res.status(200).json(product)
         } catch (error: Error | any | unknown) {
+
             console.log('Message error: ', error.message)
             res.status(400).send({message: error.message})
         }
+    }
+
+    public async deleteById(req: Request, res: Response) {
+        const { id } = req.params
+
+        try {
+            await productService.deleteById(Number(id))
+            res.status(200).send({message: 'Produto deletado com sucesso!'})
+
+        } catch (error: Error | any | unknown) {
+            console.log('message error: ', error.message);
+            res.status(400).send({ message: error.message })
         }
+    }
 }
