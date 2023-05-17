598df391f67782f8c1a78d794b29ae4452cb97d1
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-17 13:10:39 -0300

    feat: calling getById method in Router

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,8 +3,10 @@ import ProductController from '../controllers/products'
 
 const ProductRouter = Router()
 
+
 ProductRouter
   .post('/product', ProductController.create)
   .get('/product', ProductController.findAll)
+  .get('/product/:id', ProductController.getById)
 
 export default ProductRouter
