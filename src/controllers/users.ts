bab723663717af24cc125cf1350def0bed6ae62d
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-22 15:30:36 -0300

    feat: method deletebyid in usercontroller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -17,4 +17,31 @@ export default class UserController {
         }
     }
 
+    public async findAll(req: Request, res: Response) {
+        const product = await userService.getAll()
+
+        res.status(200).json(product)
+    }
+
+    public async findById(req: Request, res: Response) {
+        try {
+            const {id} = req.params;
+            const user = await userService.findById(id)
+            res.status(200).json(user)
+        } catch (error: Error | any | unknown) {
+            res.status(400).send({message: error.message})
+        }
+    }
+
+    public async deleteById(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            await userService.deleteById(id);
+            res.status(200).send({message: 'Usuario deletado'})
+        } catch (error: Error | any | unknown) {
+            console.log('message error: ', error.message);
+            res.status(400).send({ message: error.message })
+        }
+    }
+
 }
