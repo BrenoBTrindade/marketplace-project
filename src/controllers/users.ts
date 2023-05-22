d28c8d4b6abc12f8c3e3c0c1e020045ac11b5f10
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-22 18:30:27 -0300

    feat: method update for users in controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -33,6 +33,15 @@ export default class UserController {
         }
     }
 
+    public async update(req: Request, res: Response) {
+        try {
+            const user = await userService.update(req.body);
+            res.status(200).json(user)
+        } catch (error: Error | any | unknown) {
+            res.status(400).send({ message: error.message })
+        }
+    }
+
     public async deleteById(req: Request, res: Response) {
         const { id } = req.params
         try {
